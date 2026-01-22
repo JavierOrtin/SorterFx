c8f251a0822e0f2f261871d47576df020bba3953
JavierOrtin <javier.ortin.rodenas@gmail.com>
2026-01-22 21:38:32 +0100

    GH pages and react preview discrepancies

--- guide-react/src/components/ImgGuide.tsx
+++ guide-react/src/components/ImgGuide.tsx
@@ -1,5 +1,6 @@
 
 export default function ImgGuide({path, caption, alt} : {path : string, caption:string, alt:string}) {
+    if(path.charAt(0) !== "/") path = `/${path}`;
     return (
         <figure>
             <img src={`.${path}`} alt={alt}/>
